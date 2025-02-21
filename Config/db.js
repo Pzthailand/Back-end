7b9d378a324b1924066e693822ec75817fd25721
Pzthailand <Pzthailand@outlook.com>
2025-02-21 14:09:43 +0700

    update mongodb

--- Config/db.js
+++ Config/db.js
@@ -1,8 +1,20 @@
 const mongoose = require('mongoose')
 
-const connectDB = async() => {
+/*const connectDB = async() => {
     try{
         await mongoose.connect('mongodb://127.0.0.1/Pzthailand')
+        console.log('connect')
+    } catch (err){
+        console.log(err)
+    }
+}
+
+module.exports = connectDB*/
+
+
+const connectDB = async() => {
+    try{
+        await mongoose.connect('mongodb+srv://pzthailand:jXnOuyvPB3bYaESv@cluster0.kbqly.mongodb.net/')
         console.log('connect')
     } catch (err){
         console.log(err)
