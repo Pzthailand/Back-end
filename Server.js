2fa30e3f744c5b5208ddfbae205cbe4e919bda9d
Pzthailand <Pzthailand@outlook.com>
2025-02-22 10:14:54 +0700

    update

--- Server.js
+++ Server.js
@@ -16,7 +16,7 @@ const app = express();
 //app.use(cors())
 app.use(cors({
     /*origin: 'http://localhost:5173',*/
-    origin: 'https://bucolic-banoffee-15fb8f.netlify.app',// Specify the allowed origin
+    origin: 'bucolic-banoffee-15fb8f.netlify.app',// Specify the allowed origin
     credentials: true
 }));
 
